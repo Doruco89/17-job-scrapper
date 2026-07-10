6f19144ef7364768ea9d7359ab82a56975620a66
Doruco89 <ryu4746@gmail.com>
2026-07-10 11:28:57 +0900

    0710 11:28

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request
-from scrapper import search_incurit
+from scrapper import search_incurit, search_work24
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # 최초 루트페이지에 대한 설정 / 첫페이지 기본값?
 def hello_world():
     return render_template("index.html")
 
 @app.route("/search")
 def search():
-    keyword = request.args.get("keyword:")
+    keyword = request.args.get("keyword")
     print(keyword)
-    jobs = search_incurit(keyword)
-    print(jobs)
-    return render_template("search.html", jobs=enumerate(jobs))
+    jobs1 = search_incurit(keyword, 2)
+    jobs2 = search_work24(keyword)
+    job = jobs1 + jobs2
+    print(job)
+    return render_template("search.html", jobs=enumerate(job), keyword=keyword, count=len(job))
 
-
+@app.route("/file")
+def file():
+    return "file"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -1,22 +1,71 @@
 import requests
+
 from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+}
 
-def search_incurit(keyword):
-    url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
-    r = requests.get(url) #r은 변수
-    # print(r.text)
-    soup = BeautifulSoup(r.text, "html.parser")
-    lis = soup.find_all("li", class_="c_col")
-
+def search_incurit(keyword, page):
+    
     jobs = []
+    
+    for i in range(page):
+        page = 30 * i
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+        r = requests.get(url) #r은 변수
+        # print(r.text)
+        soup = BeautifulSoup(r.text, "html.parser")
+        lis = soup.find_all("li", class_="c_col")
 
-    for li in lis:
-        company = li.find("a", class_="cpname").text
-        title = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text
-        location = li.find("div", class_="cl_md").find_all("span")[0].text
-        link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
         
+        for li in lis:
+            company = li.find("a", class_="cpname").text
+            title = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text
+            location = li.find("div", class_="cl_md").find_all("span")[0].text
+            link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
+            
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link":link
+            }
+
+            jobs.append(job_data)
+    
+    return(jobs)
+
+# if __name__ == "__main__":
+#     result = search_incurit("간호사", 2)
+#     print(result)
+#     print(len(result))
+
+
+
+
+
+
+def search_work24(keyword):
+    
+    jobs = []
+    
+    url = f"https://www.work24.go.kr/cm/f/c/0100/selectUnifySearch.do?topQuerySearchArea=tb_workinfo&topQueryData={keyword}"
+    r = requests.get(url,headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+    lis = soup.find("ul", class_="srch_list_default").find_all("li")
+
+       
+    
+    for li in lis:
+        company = li.find("dl", class_= "dl_list").find("strong", class_="b1_sb").text
+        title = li.find("a", class_="btn_txt").text.strip()
+        # title = " ".join(title_tag.text.split())
+        location = li.find("div", class_="vline_group").find_all("span")[4].text.strip()
+        # location = li.find("div", class_="item").find_all("span")[0].text
+        link = li.find("dd").find("a").get("href")
+        # link = "http://www.work24.go.kr/" + li.find("dl", class_="dl_list").find("a").get("href")
+
         job_data = {
             "company": company,
             "title": title,
@@ -25,5 +74,6 @@ def search_incurit(keyword):
         }
 
         jobs.append(job_data)
-
+    
     return(jobs)
+
